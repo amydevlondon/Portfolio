ae9051f55e42bdba27698773e2de55a62eeb0e17
Amy <amyl6086@gmail.com>
2022-03-21 19:44:15 +0000

    add react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Navbar from './components/Navbar';
-import Header from './components/Header';
+import Home from './components/Home';
 import Skills from './components/Skills';
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 export const App = () => {
   return (
     <>
-      <Navbar />
-      <Header />
-      <Skills />
-      <Projects />
-      <Contact />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-custom navbar-expand-lg fixed-top">
             <div className="container-fluid">
-                <a href="/" className="navbar-brand">AL</a>
+                <Link to="/" className="navbar-brand">AL</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="fas fa-bars" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
+                            <Link to="/" className="nav-link">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/#skills">Skills</a>
+                            <Link to="/skills" className="nav-link">Skills</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/#projects">Projects</a>
+                            <Link to="/projects" className="nav-link">Projects</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="https://www.amycv.com" target="_blank" rel="noopener noreferrer">CV</a>
+                            <a href="https://www.amycv.com" className="nav-link" target="_blank" rel="noopener noreferrer">CV</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/#contact">Connect</a>
+                            <Link to="/contact" className="nav-link">Connect</Link>
                         </li>
                     </ul>
                 </div>
