90c0a6ad35f8cacf69fb1fccd74bc3a77c45fdc8
Amy <amyl6086@gmail.com>
2022-03-24 23:17:58 +0000

    change navbar to fixed bottom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-custom navbar-expand-sm">
+        <nav className="navbar navbar-custom navbar-expand-sm fixed-bottom">
             <div className="container-fluid">
                 <NavLink to="/" className="navbar-brand">A</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
