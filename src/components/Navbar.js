310a86b94160f561a43d93a9d32813f3a5b456fa
Amy <amyl6086@gmail.com>
2022-03-24 17:22:53 +0000

    add NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-custom navbar-expand-sm fixed-top">
+        <nav className="navbar navbar-custom navbar-expand-sm">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand">A</Link>
+                <NavLink to="/" className="navbar-brand">A</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="fas fa-bars" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link">Home</Link>
+                            <NavLink to="/" className="nav-link">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to="/skills" className="nav-link">Skills</Link>
+                            <NavLink to="/skills" className="nav-link">Skills</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to="/projects" className="nav-link">Projects</Link>
+                            <NavLink to="/projects" className="nav-link">Projects</NavLink>
                         </li>
                         <li className="nav-item">
                             <a href="https://www.amycv.com" className="nav-link" target="_blank" rel="noopener noreferrer">CV</a>
                         </li>
                         <li className="nav-item">
-                            <Link to="/contact" className="nav-link">Contact</Link>
+                            <NavLink to="/contact" className="nav-link">Contact</NavLink>
                         </li>
                     </ul>
                 </div>
